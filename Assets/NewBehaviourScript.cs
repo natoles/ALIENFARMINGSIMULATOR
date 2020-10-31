2964337a01f33a6f1ff6bb59b1922311c1cff0f2
loulouleboss <73760902+loulouleboss@users.noreply.github.com>
2020-11-01 00:07:40 +0100

    Boule rouge qui bouge
    DANS LE SEULE COMPORTEMENT J'AI FAIT DES TRUCS POUR BOUGER LA BOULE EN PLUS ON PEUX CHOISIR LA VITESSE AVEC UN SLIDER ET TOUT C4EST TROP COOL MERCI ANATOLE

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [Range(0, 0.1f)]
+     public float vitesseDeplacement = 0.01f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        #region C'EST ICI QU'ON RENTRE LES TOUCHES POUR BOUGER LE PETIT ROND
+        // GAUCHE DROITE
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position = new Vector3(transform.position.x - vitesseDeplacement, transform.position.y);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position = new Vector3(transform.position.x + vitesseDeplacement, transform.position.y);
+        }
+
+        //HAUT BAS
+        if (Input.GetKey(KeyCode.UpArrow)) //HAUT
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + vitesseDeplacement);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow)) //BAS
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - vitesseDeplacement);
+        }
+        #endregion
+    }
+}
